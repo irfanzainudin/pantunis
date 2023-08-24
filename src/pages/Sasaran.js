886a26752132a9d3eda151ad6a503ca201424f22
irfanzainudin <m.irfan.zain@gmail.com>
2023-08-25 00:00:07 +1000

    worked on Sasaran

--- src/pages/Sasaran.js
+++ src/pages/Sasaran.js
@@ -1,9 +1,17 @@
+import "./Sasaran.css";
+
 function Sasaran() {
   return (
     <main className="App-main">
       <div>
         <h1>Sasaran</h1>
         <p>🚧 Under Construction 🚧</p>
+        <span>
+          <big>
+            <b>1,000,000,000</b>
+          </big>{" "}
+          <p>pantun dalam pangkalan data</p>
+        </span>
       </div>
     </main>
   );
