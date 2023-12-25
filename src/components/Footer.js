02aed64d2f6c71e03a4c9bbdc90921e9476a14ee
irfanzainudin <m.irfan.zain@gmail.com>
2023-12-26 00:50:20 +1100

    make Footer fixed at the bottom of the page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,8 @@
+import "./Footer.css";
+
 function Footer() {
   return (
-    <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 px-3 border-top">
+    <footer className="d-flex flex-wrap justify-content-between align-items-end py-3 px-3 border-top">
       <div className="col-md-4 d-flex align-items-center">
         <a
           href="/"
