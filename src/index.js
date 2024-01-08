e6c065dd40ef5736975f809fba4e81503159c1bf
irfanzainudin <m.irfan.zain@gmail.com>
2024-01-08 23:53:44 +1100

    added @vercel/analytics

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import { inject } from "@vercel/analytics";
+
+inject();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
