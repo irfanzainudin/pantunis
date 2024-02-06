008208d99cdbf4f35d796450843a0b9ed44a065b
irfanzainudin <m.irfan.zain@gmail.com>
2024-02-06 21:38:07 +1100

    added sources from the Internet

---
+++ src/pages/SumberInternet.js
@@ -0,0 +1,85 @@
+function SumberInternet() {
+  return (
+    <main className="p-2 text-start">
+      <h1>
+        <i class="bi bi-globe2"></i> Pantun Daripada Internet
+      </h1>
+      <ol>
+        <li>
+          <a href="https://www.scribd.com/doc/256757965/pantun-2-kerat">
+            Scribd
+          </a>
+        </li>
+        <li>
+          <a href="https://www.youtube.com/watch?v=5_lScyhiHRE">Youtube #1</a>
+        </li>
+        <li>
+          <a href="https://id.scribd.com/doc/265151233/Pantun-Dua-Kerat">
+            Scribd
+          </a>
+        </li>
+        <li>
+          <a href="https://www.liveworksheets.com/w/ms/bahasa-melayu-bm/1006695">
+            Liveworksheets
+          </a>
+        </li>
+        <li>
+          <a href="https://www.liveworksheets.com/w/ms/bahasa-melayu-bm/1461228">
+            Liveworksheets
+          </a>
+        </li>
+        <li>
+          <a href="https://anyflip.com/ihtdm/fach/basic">Anyflip</a>
+        </li>
+        <li>
+          <a href="https://anyflip.com/ihtdm/nbna/basic">Anyflip</a>
+        </li>
+        <li>
+          <a href="ramlibahasa.blogspot.com/2013/04/jenis-jenis-pantun-pantun-2-kerat.html">
+            Ramli Bahasa's Blogspot
+          </a>
+        </li>
+        <li>
+          <a href="https://www.pinterest.com/pin/742531057309232517/?amp_client_id=CLIENT_ID%28_%29&mweb_unauth_id=&amp_url=https%3A%2F%2Fwww.pinterest.com%2Famp%2Fpin%2F742531057309232517%2F&amp_expand=true">
+            Pinterest
+          </a>
+        </li>
+        <li>
+          <a href="https://www.facebook.com/kesatuanmelayurayaII/photos/a.320864475140249/439926013234094/?type=3">
+            Facebook
+          </a>
+        </li>
+        <li>
+          <a href="https://www.youtube.com/watch?v=hrqCEO_67UM">Youtube</a>
+        </li>
+        <li>
+          <a href="https://pikaramli.wordpress.com/2019/04/10/bbm3410duapantun-dua-kerat-instagram/">
+            Pika Ramli's Wordpress
+          </a>
+        </li>
+        <li>
+          <a href="https://puisipoesy.blogspot.com/2006/08/pantun.html">
+            Puisi Poesy's Blogspot
+          </a>
+        </li>
+        <li>
+          <a href="https://thebookaholic.blogspot.com/2005/10/six-pantuns-for-lost-love.html">
+            TheBookaholic's Blogspot
+          </a>
+        </li>
+        <li>
+          <a href="https://www.bisnis.com/read/20231103/638/1710862/34-contoh-pantun-jenaka-4-baris-struktur-dan-maknanya">
+            Bisnis
+          </a>
+        </li>
+        <li>
+          <a href="https://www.cnnindonesia.com/edukasi/20230713154922-569-973098/25-contoh-pantun-jenaka-4-baris-lucu-dan-menghibur">
+            CNN Indonesia
+          </a>
+        </li>
+      </ol>
+    </main>
+  );
+}
+
+export default SumberInternet;
