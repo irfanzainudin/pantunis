07bb0d51136f2c4c1c4da3e4b52017a92cbf8814
irfanzainudin <m.irfan.zain@gmail.com>
2024-02-06 21:43:55 +1100

    added support for 'Coming Soon' sources

--- src/components/KadSumber.js
+++ src/components/KadSumber.js
@@ -1,13 +1,13 @@
 import { Link } from "react-router-dom";
 
-function KadSumber({ tajuk, penulis, pautan, imej }) {
+function KadSumber({ tajuk, penulis, pautan, imej, nota }) {
   const sumber_info = {
     tajuk: tajuk,
     penulis: penulis,
     pautan: pautan,
     imej: imej,
   };
-  return (
+  return nota === "published" ? (
     <div className="card mb-3 sumber">
       <div className="row g-0">
         <div className="col-md-4">
@@ -16,6 +16,34 @@ function KadSumber({ tajuk, penulis, pautan, imej }) {
         <div className="col-md-8">
           <div className="card-body">
             <h5 className="card-title">{tajuk}</h5>
+            <p className="card-text">{penulis}</p>
+            <p className="card-text">
+              <small className="text-body-secondary">
+                Pautan utama: <a href={pautan}>{pautan}</a>
+              </small>
+            </p>
+            <Link
+              to={"/sumber/" + tajuk.toLowerCase().replaceAll(" ", "-")}
+              className="btn btn-primary"
+              state={sumber_info}
+            >
+              Ketahui lebih
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  ) : (
+    <div className="card mb-3 sumber">
+      <div className="row g-0">
+        <div className="col-md-4">
+          <img src={imej} className="img-fluid rounded-start" alt={imej} />
+        </div>
+        <div className="col-md-8">
+          <div className="card-body">
+            <h5 className="card-title">
+              {tajuk} <span className="badge bg-secondary">Akan Datang</span>
+            </h5>
             <p className="card-text">{penulis}</p>
             <p className="card-text">
               <small className="text-body-secondary">
