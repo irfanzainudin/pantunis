2072f9edaba524b6c1b8e65283d50945d4ad293c
Irfan Zainudin <m.irfan.zain@gmail.com>
2024-10-01 16:42:44 +0800

    added Bootstrap  class around Tema for dark mode

--- src/components/Tema.js
+++ src/components/Tema.js
@@ -2,12 +2,14 @@ import { Link } from "react-router-dom";
 
 function Tema({ tema_id, nama_tema }) {
   return (
-    <Link
-      to={"/teroka/" + nama_tema.toLowerCase().split(" ").join("-")}
-      state={{ tema_id, nama_tema }}
-    >
-      <div className="kad">{nama_tema}</div>
-    </Link>
+    <div className="card m-3">
+      <Link
+        to={"/teroka/" + nama_tema.toLowerCase().split(" ").join("-")}
+        state={{ tema_id, nama_tema }}
+      >
+        <div className="kad">{nama_tema}</div>
+      </Link>
+    </div>
   );
 }
 
